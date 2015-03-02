c6182dfd4866ce64a7bf328ad5a91e2450744518
Albert Martin <albertmartin@mac.com>
2015-03-01 21:27:09 -0500

    Added metabox for embedding podcast media.

--- bethel-connector.admin.php
+++ bethel-connector.admin.php
@@ -22,6 +22,21 @@ class Bethel_Admin {
     register_setting('bethel_settings', 'bethel_settings');
 
     add_settings_section(
+      'bethel_general_section', 
+      'General settings', 
+      nil, 
+      'bethel_settings'
+    );
+
+    add_settings_field( 
+      'bethel_ministry', 
+      'API Key', 
+      array($this, 'ministry_render'), 
+      'bethel_settings', 
+      'bethel_general_section' 
+    );
+
+    add_settings_section(
       'bethel_podcast_section', 
       'Podcast Connector settings', 
       array($this, 'podcast_section'), 
@@ -35,10 +50,26 @@ class Bethel_Admin {
       'bethel_settings', 
       'bethel_podcast_section' 
     );
+
+    add_settings_field( 
+      'bethel_podcast_embed_field', 
+      'Embedded podcast player', 
+      array($this, 'podcast_embed_field_render'), 
+      'bethel_settings', 
+      'bethel_podcast_section' 
+    );
   }
 
   public function podcast_section() {
     echo 'Connect your Bethel podcast with your website.';
+  }
+
+  public function ministry_render() { 
+    $options = get_option('bethel_settings');
+
+?>
+  <input name="bethel_settings[bethel_ministry]" type="text" value="<?php echo $options['bethel_ministry']; ?>" />
+<?php
   }
 
   public function podcast_taxonomy_render() { 
@@ -47,11 +78,27 @@ class Bethel_Admin {
     $disabled_taxonomies = array('nav_menu', 'link_category', 'post_format');
 
 ?>
-  <select name='bethel_settings[bethel_podcast_taxonomy]'>
+  <select name="bethel_settings[bethel_podcast_taxonomy]">
     <?php foreach (get_taxonomies() as $tax): if (in_array($tax, $disabled_taxonomies)) continue; ?>
-    <option value="<?php echo $tax ?>" <?php selected( $options['bethel_podcast_taxonomy'], $tax ); ?> /><?php echo ucwords(str_replace('_', ' ', $tax)); ?></option>
+    <option value="<?php echo $tax ?>" <?php selected($options['bethel_podcast_taxonomy'], $tax); ?> /><?php echo ucwords(str_replace('_', ' ', $tax)); ?></option>
     <?php endforeach; ?>
   </select>
+  <p class="description">The taxonomy that will be used to autocomplete sermon series.</p>
+<?php
+
+  }
+
+  public function podcast_embed_field_render() { 
+
+    $options = get_option('bethel_settings');
+
+?>
+  <select name="bethel_settings[bethel_podcast_embed_field]">
+    <?php foreach (get_post_types() as $post): ?>
+    <option value="<?php echo $post ?>" <?php selected($options['bethel_podcast_embed_field'], $post); ?> /><?php echo ucwords(str_replace('_', ' ', $post)); ?></option>
+    <?php endforeach; ?>
+  </select>
+  <p class="description">Show the embedded podcast field on this post type.</p>
 <?php
 
   }
@@ -63,8 +110,8 @@ class Bethel_Admin {
     <form action='options.php' method='post'>
       <h2>Bethel Connector</h2>
     <?php
-      settings_fields( 'bethel_settings' );
-      do_settings_sections( 'bethel_settings' );
+      settings_fields('bethel_settings');
+      do_settings_sections('bethel_settings');
       submit_button();
     ?>
     </form>
--- bethel-connector.podcast.php
+++ bethel-connector.podcast.php
@@ -3,8 +3,10 @@
 class Bethel_Podcast {
   
   public function __construct() {
-    add_filter('query_vars', array($this, 'add_query_vars'), 0);
-    add_action('init', array($this, 'add_endpoint'), 0);
+    add_filter('query_vars', array($this, 'add_query_vars'));
+    add_action('init', array($this, 'add_endpoint'));
+    add_action('add_meta_boxes', array($this, 'add_meta_box'));
+    add_action('save_post', array($this, 'podcast_meta_box_save'));
   } 
   
   public function add_query_vars($vars) {
@@ -27,6 +29,67 @@ class Bethel_Podcast {
     echo json_encode(array('results' => $this->format_results($matches)));
 
     exit();
+  }
+
+  public function add_meta_box() {
+    $options = get_option('bethel_settings');
+    if (empty($options['bethel_podcast_embed_field']))
+      return;
+
+    add_meta_box('my-meta-box-id', 'Embedded Podcast Media', array($this, 'podcast_meta_box'), $options['bethel_podcast_embed_field'], 'normal', 'high' );
+  }
+
+  public function podcast_meta_box($post) {
+    $options = get_option('bethel_settings');
+    if (empty($options['bethel_ministry']))
+      return;
+
+    $podcasts = wp_remote_get('https://my.bethel.io/podcast/list/' . $options['bethel_ministry'] . '?episodes=true');
+    $podcasts = json_decode(wp_remote_retrieve_body($podcasts));
+
+    if (empty($podcasts)) {
+?>
+  <p>There was an error communicating with the Bethel platform. <a href="https://mybethel.zendesk.com/hc/en-us/requests/new" target="_blank">File a ticket.</a></p>
+<?php
+      return;
+    }
+
+    $values = get_post_custom($post->ID);
+    $values = unserialize($values['embedded_podcast'][0]);
+?>
+  <table cellspacing="10">
+    <thead>
+      <tr>
+        <th align="left">Podcast</th>
+        <th align="left">Episode to Embed</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+    foreach ($podcasts as $podcast) {
+?>
+      <tr>
+        <td><?php echo $podcast->name; ?></td>
+        <td>
+          <select name="embedded_podcast[<?php echo $podcast->id; ?>]">
+            <option value="">select an episode...</option>
+            <?php foreach ($podcast->media as $media): ?>
+            <option value="<?php echo $media->id ?>" <?php selected($values[$podcast->id], $media->id); ?> /><?php echo $media->name; ?></option>
+            <?php endforeach; ?>
+          </select>
+        </td>
+      </tr>
+<?php } ?>
+    </tbody>
+  </table>
+<?php
+  }
+
+  public function podcast_meta_box_save($post_id) {
+    if (!current_user_can( 'edit_post' ) || (defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE)) return;
+    
+    if(isset($_POST['embedded_podcast']))
+      update_post_meta($post_id, 'embedded_podcast', $_POST['embedded_podcast']);
   }
 
   protected function format_results($matches) {
